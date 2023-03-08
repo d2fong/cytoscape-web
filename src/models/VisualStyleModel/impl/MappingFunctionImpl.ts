07894b4ff05c8fccb3ba748a2c5dea5f28131686
Dylan Fong <dylanfong.ut@gmail.com>
2023-03-07 22:28:41 -0500

    continue export cx implementation - export network attributes by parsing the ndex network summary model - move mapping function converters to the cxVisualPropertyConverter module

--- src/models/VisualStyleModel/impl/MappingFunctionImpl.ts
+++ src/models/VisualStyleModel/impl/MappingFunctionImpl.ts
@@ -1,19 +1,6 @@
 import { ValueTypeName } from '../../TableModel'
 import { SingleValueType } from '../../TableModel/ValueType'
-import {
-  ContinuousMappingFunction,
-  MappingFunctionType,
-  VisualPropertyValueTypeName,
-  DiscreteMappingFunction,
-  PassthroughMappingFunction,
-} from '..'
-
-import {
-  CXContinuousMappingFunction,
-  CXDiscreteMappingFunction,
-  CXPassthroughMappingFunction,
-  CXVisualPropertyValue,
-} from './cxVisualPropertyConverter'
+import { MappingFunctionType, VisualPropertyValueTypeName } from '..'
 
 const valueType2BaseType: Record<ValueTypeName, SingleValueType | null> = {
   [ValueTypeName.String]: 'string',
@@ -57,79 +44,4 @@ export const typesCanBeMapped = (
   }
 
   return true
-}
-
-export const convertPassthroughMappingToCX = (
-  mapping: PassthroughMappingFunction,
-): CXPassthroughMappingFunction => {
-  const { attribute } = mapping
-
-  return {
-    type: 'PASSTHROUGH',
-    definition: {
-      attribute,
-    },
-  }
-}
-
-export const convertDiscreteMappingToCX = (
-  mapping: DiscreteMappingFunction,
-): CXDiscreteMappingFunction<CXVisualPropertyValue> => {
-  const { vpValueMap, attribute } = mapping
-
-  return {
-    type: 'DISCRETE',
-    definition: {
-      attribute,
-      map: Array.from(vpValueMap.entries()).map(([value, vpValue]) => ({
-        v: value,
-        vp: vpValue,
-      })),
-    },
-  }
-}
-export const convertContinuousMappingToCX = (
-  mapping: ContinuousMappingFunction,
-): CXContinuousMappingFunction<CXVisualPropertyValue> => {
-  const { min, max, controlPoints, attribute } = mapping
-
-  const intervals = []
-
-  for (let i = 0; i < controlPoints.length - 1; i++) {
-    const curr = controlPoints[i]
-    const next = controlPoints[i + 1]
-
-    if (curr != null && next != null) {
-      intervals.push({
-        min: curr.value as number,
-        max: next.value as number,
-        minVPValue: curr.vpValue,
-        maxVPValue: next.vpValue,
-        includeMin: curr.inclusive ?? true,
-        includeMax: next.inclusive ?? true,
-      })
-    }
-  }
-
-  return {
-    type: 'CONTINUOUS',
-    definition: {
-      map: [
-        {
-          max: min.value as number,
-          maxVPValue: min.vpValue,
-          includeMax: min.inclusive ?? true,
-          includeMin: true, // dummy value, not actually used here
-        },
-        ...intervals,
-        {
-          min: max.value as number,
-          minVPValue: max.vpValue,
-          includeMin: max.inclusive ?? true,
-          includeMax: true, // dummy value, not actually used here
-        },
-      ],
-      attribute,
-    },
-  }
 }
